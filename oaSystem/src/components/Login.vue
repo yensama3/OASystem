45d9c779eff19d60751939e2b9f6577cdb364020
Yen <495349549@qq.com>
2019-04-01 23:30:04 +0800

    前端用户名密码格式验证

--- oaSystem/src/components/Login.vue
+++ oaSystem/src/components/Login.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="Login">
-    <el-form :model="ruleForm2" status-icon :rules="rules2" ref="ruleForm2" label-width="100px" class="demo-ruleForm">
+		
+    <el-form :model="loginForm" status-icon :rules="rules" ref="loginForm" label-width="100px" class="demo-ruleForm">
+			<h1 class="title">OA系统登陆</h1>
+  <el-form-item label="用户名" prop="username">
+    <el-input type="text" v-model="loginForm.username" autocomplete="off"></el-input>
+  </el-form-item>
   <el-form-item label="密码" prop="pass">
-    <el-input type="password" v-model="ruleForm2.pass" autocomplete="off"></el-input>
+    <el-input type="password" v-model="loginForm.pass" autocomplete="off"></el-input>
   </el-form-item>
-  <el-form-item label="确认密码" prop="checkPass">
-    <el-input type="password" v-model="ruleForm2.checkPass" autocomplete="off"></el-input>
-  </el-form-item>
-  <el-form-item label="年龄" prop="age">
-    <el-input v-model.number="ruleForm2.age"></el-input>
-  </el-form-item>
+  
   <el-form-item>
-    <el-button type="primary" @click="submitForm('ruleForm2')">提交</el-button>
-    <el-button @click="resetForm('ruleForm2')">重置</el-button>
+    <el-button type="primary" @click="submitForm('loginForm')">提交</el-button>
+    
   </el-form-item>
 </el-form>
     
@@ -22,56 +22,40 @@
 <script>
   export default {
     data() {
-      var checkAge = (rule, value, callback) => {
+      var checkUsername = (rule, value, callback) => {
         if (!value) {
-          return callback(new Error('年龄不能为空'));
+          return callback(new Error('用户名不能为空'));
         }
         setTimeout(() => {
-          if (!Number.isInteger(value)) {
-            callback(new Error('请输入数字值'));
-          } else {
-            if (value < 18) {
-              callback(new Error('必须年满18岁'));
-            } else {
+         
+            
               callback();
-            }
-          }
+            
+          
         }, 1000);
       };
       var validatePass = (rule, value, callback) => {
         if (value === '') {
           callback(new Error('请输入密码'));
         } else {
-          if (this.ruleForm2.checkPass !== '') {
-            this.$refs.ruleForm2.validateField('checkPass');
-          }
+          
           callback();
         }
       };
-      var validatePass2 = (rule, value, callback) => {
-        if (value === '') {
-          callback(new Error('请再次输入密码'));
-        } else if (value !== this.ruleForm2.pass) {
-          callback(new Error('两次输入密码不一致!'));
-        } else {
-          callback();
-        }
-      };
+      
       return {
-        ruleForm2: {
+        loginForm: {
           pass: '',
           checkPass: '',
           age: ''
         },
-        rules2: {
+        rules: {
           pass: [
             { validator: validatePass, trigger: 'blur' }
           ],
-          checkPass: [
-            { validator: validatePass2, trigger: 'blur' }
-          ],
-          age: [
-            { validator: checkAge, trigger: 'blur' }
+          
+          username: [
+            { validator: checkUsername, trigger: 'blur' }
           ]
         }
       };
@@ -110,7 +94,11 @@ html,body,#app,.Login{
 	width: 400px;
 	border: 1px solid #ccc;
 	border-radius: 4px;
-	margin: 40px 20px;
+	padding: 40px 45px 10px 30px;
+}
+.Login .title{
+	width: 100%;
+	margin: 10px 170px 45px  ;
 }
 
 </style>
